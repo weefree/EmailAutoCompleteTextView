c6fbfa85177f9911232c6fb43c2b975409b74555
wee <fmw530@163.com>
2015-11-22 11:25:58 +0800

    add new type

--- email-auto-complete-textview/src/main/java/com/boxstudio/editviewlibrary/view/EmailAutoCompleteTextView.java
+++ email-auto-complete-textview/src/main/java/com/boxstudio/editviewlibrary/view/EmailAutoCompleteTextView.java
@@ -1,8 +1,13 @@
 package com.boxstudio.editviewlibrary.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.widget.AutoCompleteTextView;
 
@@ -17,6 +22,7 @@ public class EmailAutoCompleteTextView extends AutoCompleteTextView {
     public static final int TypeAfterAt = 1;
     private  String[] mEmailArray = { "@163.com", "@126.com", "@qq.com", "@gmail.com", "@hotmail.com","@sina.com", "@yahoo.com" };
 
+    private String mLastBuffer = "";
 
     private int mType = TypeNormal;
 
@@ -61,15 +67,23 @@ public class EmailAutoCompleteTextView extends AutoCompleteTextView {
 
             @Override
             public void afterTextChanged(Editable s) {
+
                 String inputStr = s.toString();
                 String emailSuffix = "";
+
+
+
+
                 if (inputStr.contains("@")) {
                     emailSuffix = inputStr.substring(inputStr.indexOf("@"), inputStr.length());
+                    if(emailSuffix.length()>0&&mLastBuffer.length()>0&&emailSuffix.endsWith(mLastBuffer)){
+                        emailSuffix =  emailSuffix.substring(0,emailSuffix.length()-mLastBuffer.length());
+                    }
                     inputStr = inputStr.substring(0, inputStr.indexOf("@"));
                 }else{
-                    if(mType == TypeAfterAt){
-                        return;
-                    }
+//                    if(mType == TypeAfterAt){
+//                        return;
+//                    }
                 }
                 adapter.mList.clear();
                 if (inputStr.length() > 0) {
@@ -86,8 +100,29 @@ public class EmailAutoCompleteTextView extends AutoCompleteTextView {
 
                     }
                 }
-                adapter.notifyDataSetChanged();
-                EmailAutoCompleteTextView.this.showDropDown();
+                EmailAutoCompleteTextView.this.removeTextChangedListener(this);
+                if(adapter.mList.size()>0&&(s.toString().indexOf('@')!=-1)){
+                    String addSuffix = adapter.mList.get(0);
+                    mLastBuffer = addSuffix.substring(inputStr.length()+emailSuffix.length());
+                    s.clear();
+                    s.append(addSuffix);
+
+                    s.setSpan(new ForegroundColorSpan(Color.GRAY), s.length() - mLastBuffer.length(), s.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    EmailAutoCompleteTextView.this.setSelection(s.length() - mLastBuffer.length());
+
+
+                }else{
+                    if(mLastBuffer.length()>0){
+
+                        String temStr = s.subSequence(0, s.length() - mLastBuffer.length()).toString();
+                        s.clear();
+                        s.append(temStr);
+                        mLastBuffer = "";
+                    }
+                }
+                EmailAutoCompleteTextView.this.addTextChangedListener(this);
+//                adapter.notifyDataSetChanged();
+//                EmailAutoCompleteTextView.this.showDropDown();
 
             }
         });
